8c4d8bafc10d7bb02158033dcf3c3d4f2a9e0522
lukezhang <416632158@qq.com>
2020-03-17 10:02:28 +0800

    1.提交构建工具

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zlu1123/luke-blog.github.io.git master:gh-pages
+git push -f git@github.com:zlu1123/luke-blog.git master:gh-pages
 
 cd -
